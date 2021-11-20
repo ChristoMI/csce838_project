69cb071e41d5f5cf78c11e97c93c4c34f96f5fd5
tmcelmury <trentamcelmury@gmail.com>
2021-11-19 21:52:18 -0600

    BluetoothConnector should work

--- BluetoothConnector.cpp
+++ BluetoothConnector.cpp
@@ -6,58 +6,55 @@
 #include "rfcomm.h"
 #include <string>
 
-#define 
 class BluetoothConnector
 {
     public bdaddr_t address;
-    public unsigned long pktId;
-
-    public int status, sckt, client;
+    public uint32_t pktId;
+    private int status, socket, client;
 
     BluetoothConnector(bdaddr_t address)    //Constructor
     {
         this.address = address;
         this.pktId = 0;
+        this.status = -1;
+        this.socket = -1;
+        this.client = -1;
     }
     
-    int[2] acceptConn(bdaddr_t* address)  
+    void AcceptConn()  
     {
         struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
         char buf[1024] = { 0 };
-        int s, client, bytes_read;
+        int bytes_read;
         socklen_t opt = sizeof(rem_addr);
 
         // allocate socket
-        s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+        this.socket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
         // bind socket to port 1 of the first available 
         // local bluetooth adapter
         loc_addr.rc_family = AF_BLUETOOTH;
         loc_addr.rc_bdaddr = *BDADDR_ANY;
         loc_addr.rc_channel = (uint8_t) 1;
-        bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+        bind(this.socket, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 
         // put socket into listening mode
-        listen(s, 1);
+        listen(this.socket, 1);
 
         // accept one connection
-        client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+        this.client = accept(this.socket, (struct sockaddr *)&rem_addr, &opt);
 
         ba2str( &rem_addr.rc_bdaddr, buf );
         fprintf(stderr, "accepted connection from %s\n", buf);
         memset(buf, 0, sizeof(buf));
-
-        int[2] values = [s, client];
-        return values;
     }
 
-    int[2] initConn(char dest[18]) 
+    void InitConn(char dest[18]) 
     {
         struct sockaddr_rc addr = { 0 };
-        int s, status;
 
         // allocate a socket
-        s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+        this.socket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
         // set the connection parameters (who to connect to)
         addr.rc_family = AF_BLUETOOTH;
@@ -65,13 +62,10 @@ class BluetoothConnector
         str2ba( dest, &addr.rc_bdaddr );
 
         // connect to server
-        status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
-
-        int[2] identifiers = [s, status];
-        return identifiers;
+        this.status = connect(this.socket, (struct sockaddr *)&addr, sizeof(addr));
     }
 
-    pkt createPkt(string message)
+    pkt CreatePkt(string message)
     {
         pkt packet;
         pkt* ptr = &packet;
@@ -83,27 +77,28 @@ class BluetoothConnector
         return packet;
     }
 
-    void sendPkt(pkt* ptr)
+    void SendPkt(pkt* ptr)
     {
         // send a message
-        if( status == 0 ) {
-            status = write(s, "hello!", 6);
+        if( this.status == 0 ) {
+            this.status = write(this.socket, "hello!", 6);
         }
-        if( status < 0 ) perror("error");
+        if( this.status < 0 ) perror("error");
     }
 
-    pkt listenPkt(int client, char[1024] buf,)
+    pkt ListenPkt(char[1024] buf,)
     {
+        int bytes_read;
         // read data from the client
-        bytes_read = read(client, buf, sizeof(buf));
+        bytes_read = read(this.client, buf, sizeof(buf));
         if( bytes_read > 0 ) {
             printf("received [%s]\n", buf);
         }
     }
 
-    void closeConn(int client, int s)
+    void CloseConn()
     {
-        close(client);
-        close(s);
+        close(this.client);
+        close(this.socket);
     }
 }
